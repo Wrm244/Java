da3ae6bb4594a7340e91e32694bef911e2cc2ccd
wrm244 <wrm244@outlook.com>
2022-06-23 22:44:15 +0800

    输出的文件换地方

--- src/JavaEXP/exam/LiveExam/Files.java
+++ src/JavaEXP/exam/LiveExam/Files.java
@@ -2,10 +2,9 @@ package JavaEXP.exam.LiveExam;
 import java.io.*;
 
 public class Files {
-
     public void write(String[] message)throws IOException
     {
-        File file=new File("Message.txt");
+        File file=new File("src/JavaEXP/exam/LiveExam/Message.txt");//添加文件
         String messagesum="";
         for (int i=0; i<2; i++)  //将信息格式化存储
             messagesum+=message[i]+"~";
@@ -19,19 +18,17 @@ public class Files {
     }
     public int read(String countname,String pwd) throws IOException{
 
-        File file=new File("Message.txt");   //创建文件类
+        File file=new File("src/JavaEXP/exam/LiveExam/Message.txt");   //创建文件类
         if(!file.exists()||file.isDirectory()) //判断文件是否存在
-            //throw new FileNotFoundException();
             file.createNewFile();
         BufferedReader br=new BufferedReader(new FileReader(file)); //创建读入缓冲流，按行读入
         String temp;
-        // StringBuffer sb=new StringBuffer();
         int number = 0;
         temp=br.readLine();   //先读取一行
         while(temp!=null){
             String sbstring = temp.toString();   //转化为string
             int n = sbstring.length();            //测字符串长度
-            String []message = new String[5];     //按~拆分 成5个字符串数组，按账号和密码进行信息验证
+            String [] message = new String[5];     //按~拆分 成5个字符串数组，按账号和密码进行信息验证
             int k=0;
 
             for (int i=0; i<2; i++)
@@ -41,7 +38,6 @@ public class Files {
             {
                 if(sbstring.charAt(i)=='~')
                 {
-                    //System.out.println("@"+message[k]);
                     k++;
                 }
                 else
